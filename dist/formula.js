16b8b96f62c0af544ad9a5fd1eed7201b29c226a
Remi Aleksander Solås <remi@npolar.no>
2015-11-13 10:14:01 +0100

    Added JSONP as fallback for failed jsonLoader requests

--- dist/formula.js
+++ dist/formula.js
@@ -1729,39 +1729,41 @@ angular.module('formula')
  * formula.js
  * Generic JSON Schema form builder
  *
- * Norsk Polarinstutt 2014, http://npolar.no/
+ * Norsk Polarinstutt 2015, http://npolar.no/
  */
 
 angular.module('formula')
-	
+
 	/**
 	 * @factory jsonLoader
-	 * 
+	 *
 	 * Service used for asynchronous loading of JSON files.
-	 * 
+	 *
 	 * @param uri JSON file uri as a string
 	 * @returns $q promise object to requested JSON file
 	 */
-	
+
 	.factory('formulaJsonLoader',
 	['$http', '$q', 'formulaLog',
 	function($http, $q, log) {
-		return function(uri, jsonp) {
+		var jsonLoader;
+
+		return (jsonLoader = function(uri, jsonp, rerun) {
 			var deferred = $q.defer();
-			
-			if(jsonp === undefined) {
-				jsonp = (uri.search(/https?:/) === 0);
-			}
-			
+
 			(jsonp ? $http.jsonp(uri + '?callback=JSON_CALLBACK') : $http.get(uri))
 				.success(function(data, status, headers, config) {
 					deferred.resolve(data);
 				}).error(function(data, status, headers, config) {
+					if(!jsonp) {
+						return jsonLoader(uri, true);
+					}
+
 					log.error(log.codes.JSON_LOAD_ERROR, { uri: uri });
 					deferred.reject(uri);
 				});
 			return deferred.promise;
-		};
+		});
 	}]);
 
 /**
